c5d9fead41d22aaf99fccc07adde94e09777c3ed
Brad Richardson <brichardson@structint.com>
2017-02-08 12:42:37 -0800

    Start refactoring the tests

--- tests.py
+++ tests.py
@@ -3,14 +3,15 @@ import Game
 
 class testBolwingGame(unittest.TestCase):
 
+    def setUp(self):
+        self._g = Game.Game()
+
     def testGutterGame(self):
-        g = Game.Game()
         for i in xrange(20):
-            g.roll(0)
-        assert g.score() == 0
+            self._g.roll(0)
+        assert self._g.score() == 0
 
     def testAllOnes(self):
-        g = Game.Game()
         for i in xrange(20):
-            g.roll(1)
-        assert g.score() == 20
+            self._g.roll(1)
+        assert self._g.score() == 20
