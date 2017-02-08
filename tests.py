95cfac754b8b8d98f1321f83337c40a58e8ba320
Brad Richardson <brichardson@structint.com>
2017-02-08 12:25:13 -0800

    Add test for rolling all ones

--- tests.py
+++ tests.py
@@ -8,3 +8,9 @@ class testBolwingGame(unittest.TestCase):
         for i in xrange(20):
             g.roll(0)
         assert g.score() == 0
+
+    def testAllOnes(self):
+        g = Game.Game()
+        for i in xrange(20):
+            g.roll(1)
+        assert g.score() == 20
