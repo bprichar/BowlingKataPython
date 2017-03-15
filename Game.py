276d56ef5aa479ce516d7bf97ab917b871076a20
Brad Richardson <brichardson@structint.com>
2017-03-14 19:17:11 -0700

    Do some refactoring for additional clarity

--- Game.py
+++ Game.py
@@ -8,15 +8,30 @@ class Game():
 
     def score(self):
         score = 0
-        i = 0
+        frame_index = 0
         for frame in xrange(10):
-            if self._rolls[i] + self._rolls[i+1] == 10: #Spare
-                score += 10 + self._rolls[i+2]
-                i += 2
-            elif self._rolls[i] == 10: #Strike
-                score += 10 + self._rolls[i+1] + self._rolls[i+2]
-                i += 1
+            if self._isStrike(frame_index):
+                score += 10 + self._strikeBonus(frame_index)
+                frame_index += 1
+            elif self._isSpare(frame_index):
+                score += 10 + self._spareBonus(frame_index)
+                frame_index += 2
             else:
-                score += self._rolls[i] + self._rolls[i+1]
-                i += 2
+                score += self._sumOfBallsInFrame(frame_index)
+                frame_index += 2
         return score
+
+    def _isStrike(self, frame_index):
+        return self._rolls[frame_index] == 10
+
+    def _isSpare(self, frame_index):
+        return self._rolls[frame_index] + self._rolls[frame_index+1] == 10
+
+    def _strikeBonus(self, frame_index):
+        return self._rolls[frame_index+1] + self._rolls[frame_index+2]
+
+    def _spareBonus(self, frame_index):
+        return self._rolls[frame_index+2]
+
+    def _sumOfBallsInFrame(self, frame_index):
+        return self._rolls[frame_index] + self._rolls[frame_index+1]
